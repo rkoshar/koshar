75da699dc0845430f483fc5b7d7b4a6c91dd213d
Ryan Koshar <rkoshar@sbhic.com>
2014-03-26 11:52:48 -0500

    Initialize repository

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def listings
+  end
+
+  def modular
+  end
+
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Koshar::Application.config.secret_key_base = 'ef64e7d0740c206b1d1587e7bd2b8d804743d0ab77a3650f28a6421aed5ad71385e8ec98985d9377403c54e02c72a1c025ed22608896268b4e815e75baa7b7d4'
